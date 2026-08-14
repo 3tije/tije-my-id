fa3c586ece0a8b1795428b7160ff22668e21e8b7
3tije <b177tri@gmail.com>
2026-08-14 20:29:29 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE = "tije-hub-v1";
-const ASSETS = ["./","index.html","styles.css","app.js","config.js","manifest.webmanifest","assets/favicon.svg","assets/tri-tjahyono.webp"];
+const ASSETS = ["./","index.html","styles.css","app.js","config.js","manifest.webmanifest","assets/mytije-icon.svg","assets/tri-tjahyono.webp","assets/tri-formal.jpg","assets/tri-seragam.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
